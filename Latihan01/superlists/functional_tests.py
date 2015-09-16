6f9ef9aa111cc4a00fb76b66e63932aa12964b45
Gede Wahyu Adi Pramana <gedewahyu.ap@gmail.com>
2015-09-16 11:00:23 +0700

    peacock flying, input test

--- Latihan01/superlists/functional_tests.py
+++ Latihan01/superlists/functional_tests.py
@@ -3,35 +3,35 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
-	
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
-	def tearDown(self):
-		self.browser.quit()
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get('http://localhost:8000')
-		self.assertIn('Home Page', self.browser.title)
-		#self.assertIn('To-Do', self.browser.title)
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Home Page', self.browser.title)
+	#self.assertIn('To-Do', self.browser.title)
 		
-		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('To-Do', header_text)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-		inputBox = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(
-			inputBox.get_attribute('placeholder'),
-			'Enter a to-do item'
-		)
+        inputBox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputBox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 		
-		inputBox.send_keys('Buy peacock feathers')
-		inputBox.send_keys(Keys.ENTER)
+        inputBox.send_keys('Use peacock feathers to make a fly')
+        inputBox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table"
-		)
-	
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])	
+        self.assertIn(
+            '2: Use peacock feathers to make a fly' ,
+            [row.text for row in rows]
+        )
 
 if __name__ == '__main__':
-	unittest.main(warnings='ignore')
+    unittest.main(warnings='ignore')
