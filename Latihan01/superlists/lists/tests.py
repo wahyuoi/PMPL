6594a925d45346893b30dd5e0a2e008d06ac89a2
Gede Wahyu Adi Pramana <gedewahyu.ap@gmail.com>
2015-09-16 09:42:56 +0700

    Refactor home page view to use a template

--- Latihan01/superlists/lists/tests.py
+++ Latihan01/superlists/lists/tests.py
@@ -1,20 +1,15 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
-
+from django.template.loader import render_to_string
 from lists.views import home_page
 
-
 class HomePageTest(TestCase):
-
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()  #1
-        response = home_page(request)  #2
-        self.assertTrue(response.content.startswith(b'<html>'))  #3
-        self.assertIn(b'<title>Home Page</title><body>Nama : Gede Wahyu<br>NPM : 1206219073</body>', response.content)  #4
-        self.assertTrue(response.content.endswith(b'</html>'))  #5
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		expected_html = render_to_string(b'home.html')
+		self.assertEqual(response.content, expected_html)
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
--- Latihan01/superlists/lists/views.py
+++ Latihan01/superlists/lists/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-def home_page(request):
-    return HttpResponse('<html><title>Home Page</title><body>Nama : Gede Wahyu<br>NPM : 1206219073</body></html>')
 
 # Create your views here.
+def home_page(request):
+    return render(request, 'home.html')
