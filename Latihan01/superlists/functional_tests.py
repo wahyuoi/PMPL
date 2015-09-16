e05de9f02ddaeeef4b46640108b489ed147480a2
Gede Wahyu Adi Pramana <gedewahyu.ap@gmail.com>
2015-09-16 11:06:28 +0700

    two input

--- Latihan01/superlists/functional_tests.py
+++ Latihan01/superlists/functional_tests.py
@@ -8,6 +8,11 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.implicitly_wait(3)
     def tearDown(self):
         self.browser.quit()
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('Home Page', self.browser.title)
@@ -21,9 +26,16 @@ class NewVisitorTest(unittest.TestCase):
             inputBox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
-		
+        inputBox.send_keys('Buy peacock feathers')
+        inputBox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        inputBox = self.browser.find_element_by_id('id_new_item')
         inputBox.send_keys('Use peacock feathers to make a fly')
         inputBox.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
