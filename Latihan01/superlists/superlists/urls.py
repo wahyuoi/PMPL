f9df6215edeac1509a6364863143841593113eb8
Gede Wahyu Adi Pramana <gedewahyu.ap@gmail.com>
2015-09-30 06:31:10 +0700

    new URL and view for adding to existing lists

--- Latihan01/superlists/superlists/urls.py
+++ Latihan01/superlists/superlists/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 #    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home_page, name='home'),
-    url(r'lists/new', views.new_list, name='new_list'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    url(r'lists/new', 'lists.views.new_list', name='new_list'),
+    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
 ]
