6f226b5c5af103a9c4ee5ec87fa957b89f2df64a
Gede Wahyu Adi Pramana <gedewahyu.ap@gmail.com>
2015-09-30 03:42:46 +0700

    add regex checker for /list

--- Latihan01/superlists/functional_tests/tests.py
+++ Latihan01/superlists/functional_tests/tests.py
@@ -29,6 +29,8 @@ class NewVisitorTest(LiveServerTestCase):
         )
         inputBox.send_keys('Buy peacock feathers')
         inputBox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputBox = self.browser.find_element_by_id('id_new_item')
@@ -45,3 +47,28 @@ class NewVisitorTest(LiveServerTestCase):
             '2: Use peacock feathers to make a fly' ,
             [row.text for row in rows]
         )
+
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+        
+
