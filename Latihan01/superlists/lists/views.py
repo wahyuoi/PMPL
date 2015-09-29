c6f481c945e9226b65af7440d07d8f49a9e7f8a1
Gede Wahyu Adi Pramana <gedewahyu.ap@gmail.com>
2015-09-30 04:36:28 +0700

    move todo form field

--- Latihan01/superlists/lists/views.py
+++ Latihan01/superlists/lists/views.py
@@ -3,14 +3,12 @@ from django.shortcuts import render, redirect
 from lists.models import Item
 
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']  #1
-        Item.objects.create(text=new_item_text)  #2
-        return redirect('/lists/the-only-list-in-the-world/')
-    else:
-        new_item_text = ''  #3
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
